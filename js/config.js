da465e6b0147397660944cb781a4f557c20910d9
Mihir Jariwala <39168691+mihirjari@users.noreply.github.com>
2023-08-29 14:38:21 -0400

    new config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_vJAaaSwgE', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7i2g874u3l0aaha5mj7pp4mv2f', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
